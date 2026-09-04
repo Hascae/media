e9dc95edcdc510ab62fe58f65dc7f9432317e82f
Arseny271 <arsendmitr@gmail.com>
2026-09-04 12:05:47 +0400

    Update

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/ExoPlayer.java
@@ -45,6 +45,7 @@ import androidx.media3.common.PriorityTaskManager;
 import androidx.media3.common.Timeline;
 import androidx.media3.common.TrackSelectionParameters;
 import androidx.media3.common.Tracks;
+import androidx.media3.common.VideoListener;
 import androidx.media3.common.VideoSize;
 import androidx.media3.common.text.CueGroup;
 import androidx.media3.common.util.Clock;
@@ -77,6 +78,7 @@ import com.google.common.base.Function;
 import com.google.common.base.Supplier;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
 import java.util.List;
+import java.util.concurrent.Executor;
 
 /**
  * An extensible media player that plays {@link MediaSource}s. Instances can be obtained from {@link
@@ -1198,7 +1200,7 @@ public interface ExoPlayer extends Player {
       return new ExoPlayerImpl(/* builder= */ this, /* wrappingPlayer= */ null);
     }
 
-    /* package */ SimpleExoPlayer buildSimpleExoPlayer() {
+    public SimpleExoPlayer buildSimpleExoPlayer() {
       checkState(!buildCalled);
       buildCalled = true;
       return new SimpleExoPlayer(/* builder= */ this);
@@ -1839,4 +1841,17 @@ public interface ExoPlayer extends Player {
    */
   @UnstableApi
   void setImageOutput(ImageOutput imageOutput);
+
+  default void setWorkerQueue(Executor dispatchQueue) {
+
+  }
+
+  default void addVideoListener(VideoListener listener) {
+
+  }
+
+  default void removeVideoListener(VideoListener listener) {
+
+  }
+
 }
--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/mediacodec/MediaCodecRenderer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/mediacodec/MediaCodecRenderer.java
@@ -661,7 +661,7 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
   }
 
   @Nullable
-  protected final MediaFormat getCodecOutputMediaFormat() {
+  public final MediaFormat getCodecOutputMediaFormat() {
     return codecOutputMediaFormat;
   }
 
