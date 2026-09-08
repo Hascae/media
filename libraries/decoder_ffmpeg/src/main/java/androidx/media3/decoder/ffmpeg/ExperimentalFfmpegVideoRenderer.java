eed34fcf502b84c01e192144ea4d009a495f1ede
Arseny271 <arsendmitr@gmail.com>
2026-09-08 14:05:22 +0400

    FFMpeg video support

--- libraries/decoder_ffmpeg/src/main/java/androidx/media3/decoder/ffmpeg/ExperimentalFfmpegVideoRenderer.java
+++ libraries/decoder_ffmpeg/src/main/java/androidx/media3/decoder/ffmpeg/ExperimentalFfmpegVideoRenderer.java
@@ -25,7 +25,6 @@ import androidx.annotation.Nullable;
 import androidx.media3.common.C;
 import androidx.media3.common.Format;
 import androidx.media3.common.MimeTypes;
-import androidx.media3.common.util.ExperimentalApi;
 import androidx.media3.common.util.TraceUtil;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.common.util.Util;
@@ -45,7 +44,7 @@ import androidx.media3.exoplayer.video.VideoRendererEventListener;
  * <p>Decodes and renders video using FFmpeg.
  * <p>Merge actual implementation in https://github.com/google/ExoPlayer/pull/7132.
  */
-@ExperimentalApi // TODO: b/470357054 - Merge pull request and publish FfmpegVideoRenderer.
+ // TODO: b/470357054 - Merge pull request and publish FfmpegVideoRenderer.
 public final class ExperimentalFfmpegVideoRenderer extends DecoderVideoRenderer {
 
   private static final String TAG = "ExperimentalFfmpegVideoRenderer";
